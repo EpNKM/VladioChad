eb94daf64323eaeabb7151d0bbbf00e838f8d0da
Владислав Кузнецов <kuzn3tsoff.v@yandex.ru>
2025-06-24 22:53:13 +0300

    Remove build directory from Git

--- build/Desktop_Qt_6_9_0_shared_MinGW_w64_MINGW64_MSYS2-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_AuthoLASTVLADIO_FILE S:/Qt Project/AuthoLASTVLADIO/build/Desktop_Qt_6_9_0_shared_MinGW_w64_MINGW64_MSYS2-Release/AuthoLASTVLADIO.exe)
-set(__QT_DEPLOY_TARGET_AuthoLASTVLADIO_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_AuthoLASTVLADIO_RUNTIME_DLLS C:/msys64/mingw64/bin/Qt6MultimediaWidgets.dll;C:/msys64/mingw64/bin/Qt6Widgets.dll;C:/msys64/mingw64/bin/Qt6Multimedia.dll;C:/msys64/mingw64/bin/Qt6Network.dll;C:/msys64/mingw64/bin/Qt6Gui.dll;C:/msys64/mingw64/bin/Qt6Core.dll)
